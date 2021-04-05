9acf8b4ec67a6c0ab1fd5f8b3b6a9166bc7b7a55
nishida <i9i9ksy@gmail.com>
2021-04-05 23:04:08 +0900

    QiitaのAPIでエンドポイント作成

--- functions/index.js
+++ functions/index.js
@@ -1,53 +1,23 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const requestPromise = require("request-promise-native"); // 追加
-const cors = require("cors"); // 追加
+const requestPromise = require("request-promise-native");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-app.use(cors()); // 追加
-
-// APIにリクエストを送る関数を定義
-const getDataFromApi = async (keyword) => {
-    // cloud functionsから実行する場合には地域の設定が必要になるため，`country=JP`を追加している
-    const requestUrl = "https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:";
+// 関数設定
+const getQiita = async (keyword) => {
+    const requestUrl = "https://qiita.com/api/v2/items?page=1&per_page=10&query=";
     const result = await requestPromise(`${requestUrl}${keyword}`);
     return result;
 };
 
-// app.get("/hello", (req, res) => {
-//     res.send("Hello Express!");
-// });
-
-app.get("/user/:userId", (req, res) => {
-    const users = [
-        { id: 1, name: "ジョナサン" },
-        { id: 2, name: "ジョセフ" },
-        { id: 3, name: "承太郎" },
-        { id: 4, name: "仗助" },
-        { id: 5, name: "ジョルノ" },
-    ];
-    const targetUser = users.find((user) => user.id === Number(req.params.userId));
-    res.send(targetUser);
-});
-
-// curl http://localhost:5000/functions-be08e/us-central1/helloWorld
-// curl https://us-central1-functions-be08e.cloudfunctions.net/api
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
 // エンドポイント追加
-app.get("/gbooks/:keyword", async (req, res) => {
-    // APIリクエストの関数を実行
-    const response = await getDataFromApi(req.params.keyword);
+app.get("/qiita/:keyword", async (req, res) => {
+    const response = await getQiita(req.params.keyword);
     res.send(response);
 });
 
 const api = functions.https.onRequest(app);
 module.exports = { api };
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
